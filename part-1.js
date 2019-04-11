96e40bc264dc056b058bef7d1b185d8351d5b62f
Hou Fai Man <HouFai.Man@ig.com>
2019-04-11 15:09:17 +0100

    Workshop

--- part-1.js
+++ part-1.js
@@ -1,1 +1,13 @@
-console.log('your code here')
+var fs = require('fs');
+var fileContents = fs.readFileSync('names.txt', 'utf-8');
+var namesArray = fileContents.split('\r\n');
+var reverseArray = namesArray.reverse();
+
+function reverseName(str) {
+    return str.split("").reverse().join("");
+}
+
+for (var name of namesArray) {
+    var reversedName = reverseName(name);
+    fs.writeFileSync(reversedName, reversedName, 'utf-8');
+}
--- part-2.js
+++ part-2.js
@@ -1,1 +1,20 @@
-console.log('your code here')
+var fs = require('fs');
+var fileContent = fs.readFileSync('shopping-basket.json', 'utf-8');
+var data = JSON.parse(fileContent);
+var total = 0;
+
+for (var item of data.basket) {
+    if (item.name === 'Candles') {
+        item.quantity = 10;
+    }
+    
+    var result = item.price * item.quantity;
+    total += result;
+    console.log(`${item.name}: ${result}`);
+}
+
+data.total = total;
+
+var jsonString = JSON.stringify(data, null, 2);
+
+fs.writeFileSync('new-basket.json', jsonString, 'utf-8');
